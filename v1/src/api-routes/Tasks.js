b4dc0f53c1490f11efb93e8ceeebc9e38b0d1aa0
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-22 18:48:34 +0300

    validasyon middleware eklendi

--- v1/src/api-routes/Tasks.js
+++ v1/src/api-routes/Tasks.js
@@ -1,7 +1,15 @@
 const { authenticateToken } = require("../middlewares/authenticatePassport");
 const express = require("express");
 const TaskController = require("../controllers/Task");
+const validate = require("../middlewares/validate");
+const schemas = require("../validations/Tasks");
 const router = express.Router();
 router.route("/").get(authenticateToken(), TaskController.getAll);
-router.route("/").post(authenticateToken(), TaskController.create);
+router
+  .route("/")
+  .post(
+    validate(schemas.createValidation),
+    authenticateToken(),
+    TaskController.create
+  );
 module.exports = router;
