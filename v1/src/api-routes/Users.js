7d060ee788f4d686fdb77cbf10229fc12bf653d2
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-22 17:03:17 +0300

    user router düzenlendi getAll test edildi çalışıyor.

--- v1/src/api-routes/Users.js
+++ v1/src/api-routes/Users.js
@@ -2,9 +2,7 @@ const validate = require("../middlewares/validate");
 const { authenticateToken } = require("../middlewares/authenticatePassport");
 const schemas = require("../validations/Auths");
 const express = require("express");
-const AuthController = require("../controllers/Auth");
+const UserController = require("../controllers/User");
 const router = express.Router();
-router
-  .route("/list")
-  .get(validate(schemas.createValidation), AuthController.register);
+router.route("/list").get(UserController.getAll);
 module.exports = router;
