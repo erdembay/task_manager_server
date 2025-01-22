7586d1f5201e8617e483a0e036a38031c9188eb1
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-22 17:44:53 +0300

    task controllerlar oluşturuldu

--- v1/src/controllers/Task.js
+++ v1/src/controllers/Task.js
@@ -1,4 +1,46 @@
 const httpStatus = require("http-status");
 const ApiError = require("../errors/ApiError");
-class Tasks {}
+const TaskService = require("../services/MongoService/TaskService");
+class Tasks {
+  async getAll(req, res, next) {
+    try {
+      res.status(httpStatus.OK).send({
+        status: true,
+        message: "Tasks Get All",
+      });
+    } catch (error) {
+      next(new ApiError(error?.message));
+    }
+  }
+  async create(req, res, next) {
+    try {
+      res.status(httpStatus.OK).send({
+        status: true,
+        message: "Tasks Create",
+      });
+    } catch (error) {
+      next(new ApiError(error?.message));
+    }
+  }
+  async update(req, res, next) {
+    try {
+      res.status(httpStatus.OK).send({
+        status: true,
+        message: "Tasks Update",
+      });
+    } catch (error) {
+      next(new ApiError(error?.message));
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      res.status(httpStatus.OK).send({
+        status: true,
+        message: "Tasks Delete",
+      });
+    } catch (error) {
+      next(new ApiError(error?.message));
+    }
+  }
+}
 module.exports = new Tasks();
