a9ab92f7e44c8e99c12f0d8f0bc8e0c52d4c1d54
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-22 16:02:17 +0300

    attachment için tablo oluşturuldu dotenv konumu manuel verildi

--- v1/src/config/config.js
+++ v1/src/config/config.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../../.env" });
 module.exports = {
   development: {
     username: process.env.MYSQL_DB_USER || "root",
-    password: process.env.MYSQL_DB_PASS || null,
+    password: process.env.MYSQL_DB_PASS || "12345678",
     database: process.env.MYSQL_DB_NAME || "task_manager",
     host: process.env.MYSQL_DB_HOST || "localhost",
     dialect: "mysql",
   },
   test: {
     username: process.env.MYSQL_DB_USER || "root",
-    password: process.env.MYSQL_DB_PASS || null,
+    password: process.env.MYSQL_DB_PASS || "12345678",
     database: process.env.MYSQL_DB_NAME || "task_manager",
     host: process.env.MYSQL_DB_HOST || "localhost",
     dialect: "mysql",
   },
   production: {
     username: process.env.MYSQL_DB_USER || "root",
-    password: process.env.MYSQL_DB_PASS || null,
+    password: process.env.MYSQL_DB_PASS || "12345678",
     database: process.env.MYSQL_DB_NAME || "task_manager",
     host: process.env.MYSQL_DB_HOST || "localhost",
     dialect: "mysql",
---
+++ v1/src/migrations/20250122125501-create-attachments.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("Attachments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      taskId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: "Tasks",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: true, // 'image', 'video', 'document'
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("Attachments");
+  },
+};
