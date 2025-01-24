71f8f16ee63280762456d8e75f935a760d16bdba
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-24 06:00:09 +0300

    task log şeması oluşturuldu loglama için

---
+++ v1/src/models/mongo/TaskLog.js
@@ -0,0 +1,29 @@
+const Mongoose = require("mongoose");
+// ekleme güncelleme silme işlemlerini loglamak için kullanılacak
+const TaskLogSchema = new Mongoose.Schema(
+  {
+    taskId: {
+      type: Number,
+      required: true,
+    },
+    operation: {
+      type: String,
+      required: true,
+      enum: ["CREATE", "UPDATE", "DELETE"],
+    },
+    details: {
+      type: Object,
+      default: {},
+    },
+    userId: {
+      type: Number,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+module.exports = Mongoose.model("task_log", TaskLogSchema);
